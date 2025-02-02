8a6adfc917561396c17ae43c58edd77631232cbf
Jeff Michaud <cometaj2@comcast.net>
2025-02-02 13:56:59 -0800

    update gitignore and manifest, and clear out build and dist

--- build/lib/hello/package.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-__version__ = "0.1.0"
-dependencies = ["flask==3.0.1",
-                "huckle==5.5.4",
-                "hcli_hai>=2.1.3"]
